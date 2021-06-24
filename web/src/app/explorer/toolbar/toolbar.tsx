e867d4d4ff367e8c647608182a7e879679cd6d81
Thien Do <dvkndn@gmail.com>
2021-06-25 04:33:34 +0700

    Refactor(web): ExtractToolbarOpen

--- web/src/app/explorer/toolbar/toolbar.tsx
+++ web/src/app/explorer/toolbar/toolbar.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@moai/core";
 import { Backend } from "backend/interface";
 import { PaneHeading } from "components/pane/heading/heading";
 import { TreeNode } from "components/tree/tree";
-import { RiFolderLine } from "react-icons/ri";
 import { SetState } from "utils/state";
 import { ExplorerToolbarAdd } from "./add";
+import { ExplorerToolbarOpen } from "./open";
 import s from "./toolbar.module.css";
 
 export interface ExplorerToolbarProps {
@@ -15,30 +14,10 @@ export interface ExplorerToolbarProps {
 	setRootNode: SetState<TreeNode | null>;
 }
 
-const openFolder = async (props: ExplorerToolbarProps): Promise<void> => {
-	const open = props.backend.storage.showOpenDialog;
-	if (typeof open === "string")
-		throw Error("Store doesn't support opening a folder");
-
-	const path = await open();
-	if (path === null) return; // Cancel
-
-	if (props.setRootPath === null)
-		throw Error("setRootPath is null because of fixed root mode");
-	props.setRootPath(path);
-};
-
 const Aside = (props: ExplorerToolbarProps): JSX.Element => (
 	<div className={s.aside}>
 		<ExplorerToolbarAdd {...props} />
-		{props.setRootPath !== null && (
-			<Button
-				style={Button.styles.flat}
-				onClick={() => openFolder(props)}
-				icon={RiFolderLine}
-				iconLabel="Change Folder"
-			/>
-		)}
+		<ExplorerToolbarOpen {...props} />
 	</div>
 );
 
