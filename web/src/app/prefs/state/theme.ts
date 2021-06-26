f31e6eb44731a0f16f0a2035955c83fc7fa39cfc
Thien Do <dvkndn@gmail.com>
2021-06-26 22:19:16 +0700

    Feat(web): Support Safari new theme color

--- web/src/app/prefs/state/theme.ts
+++ web/src/app/prefs/state/theme.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Theme } from "./state";
 
 const DARK_MODE_QUERY = "(prefers-color-scheme: dark)";
@@ -9,6 +9,28 @@ const resolveTheme = (theme: Theme): "light" | "dark" => {
 	return isDark ? "dark" : "light";
 };
 
+export const usePrefsResolvedTheme = (original: Theme): "light" | "dark" => {
+	const [resolved, setResolved] = useState(() => {
+		return resolveTheme(original);
+	});
+
+	useEffect(() => {
+		const resolved = resolveTheme(original);
+		setResolved(resolved);
+
+		// Watch for change if "system"
+		if (original !== "system") return;
+		const medias = window.matchMedia(DARK_MODE_QUERY);
+		const listener = (event: MediaQueryListEvent): void => {
+			setResolved(event.matches ? "dark" : "light");
+		};
+		medias.addEventListener("change", listener);
+		return () => medias.removeEventListener("change", listener);
+	}, [original]);
+
+	return resolved;
+};
+
 const updateTheme = (theme: "light" | "dark"): void => {
 	const classList = window.document.documentElement.classList;
 	if (classList.contains(theme)) return;
@@ -16,21 +38,10 @@ const updateTheme = (theme: "light" | "dark"): void => {
 	classList.add(theme);
 };
 
-export const usePrefsTheme = (theme: Theme): void => {
-	// Set class to HTML
-	useEffect(() => {
-		const resolved = resolveTheme(theme);
-		updateTheme(resolved);
-	}, [theme]);
+export const usePrefsTheme = (original: Theme): void => {
+	const resolved = usePrefsResolvedTheme(original);
 
-	// Watch for change if "system"
 	useEffect(() => {
-		if (theme !== "system") return;
-		const medias = window.matchMedia(DARK_MODE_QUERY);
-		const listener = (event: MediaQueryListEvent): void => {
-			updateTheme(event.matches ? "dark" : "light");
-		};
-		medias.addEventListener("change", listener);
-		return () => medias.removeEventListener("change", listener);
-	}, [theme]);
+		updateTheme(resolved);
+	}, [resolved]);
 };
--- web/src/app/title/native/native.tsx
+++ web/src/app/title/native/native.tsx
@@ -2,11 +2,12 @@ import { Button } from "@moai/core";
 import { File } from "app/file/file";
 import { Prefs } from "app/prefs/state/state";
 import { BackendPathUtils } from "backend/interface";
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 import { SetState } from "utils/state";
 import { getDocumentTitle } from "../document/document";
 import { TitleToolbar } from "../toolbar/toolbar";
 import s from "./native.module.css";
+import { TitleNativeTheme } from "./theme";
 
 interface Props {
 	file: File;
@@ -20,17 +21,21 @@ interface Props {
  * but render the toolbar toggle on top of the toolbar. Also take care of
  * document.title
  */
-export const TitleNative = (props: Props): JSX.Element => (
-	<>
-		<Helmet>
-			<title>{getDocumentTitle({ file: props.file, path: props.path })}</title>
-		</Helmet>
-		<div className={s.toolbar}>
-			<TitleToolbar
-				size={Button.sizes.medium}
-				prefs={props.prefs}
-				setPrefs={props.setPrefs}
-			/>
-		</div>
-	</>
-);
+export const TitleNative = (props: Props): JSX.Element => {
+	const { file, path } = props;
+	return (
+		<>
+			<Helmet>
+				<title children={getDocumentTitle({ file, path })} />
+			</Helmet>
+			<TitleNativeTheme prefs={props.prefs} />
+			<div className={s.toolbar}>
+				<TitleToolbar
+					size={Button.sizes.medium}
+					prefs={props.prefs}
+					setPrefs={props.setPrefs}
+				/>
+			</div>
+		</>
+	);
+};
