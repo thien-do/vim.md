1eeaaa4a29bd2e37fbf68d9c70e1c442fc6201ac
huyng12 <touch@nguyenrk.com>
2021-08-24 08:27:50 +0700

    Feat: Sort by file type then file name

--- web/src/app/explorer/file.ts
+++ web/src/app/explorer/file.ts
@@ -11,6 +11,16 @@ const toTreeNode =
 		children: undefined, // Don't know yet
 	});
 
+const fileNodesCompareFn = (a: TreeNode, b: TreeNode) => {
+	if (!a.isLeaf && !b.isLeaf) {
+		// they are both folders, then sort by alphabet
+		return a.label.toLowerCase().localeCompare(b.label.toLowerCase());
+	}
+
+	// folders should be displayed before files
+	return a.isLeaf ? 1 : -1;
+};
+
 const MARKDOWN_EXTENSIONS: Set<string> = new Set(["md", "mdx"]);
 
 export const listFilesAsNodes = async (params: {
@@ -21,6 +31,6 @@ export const listFilesAsNodes = async (params: {
 	const { path, fileType, list } = params;
 	const extensions = fileType === "all" ? "all" : MARKDOWN_EXTENSIONS;
 	const files = await list(path, extensions);
-	const nodes = files.map(toTreeNode(path));
+	const nodes = files.map(toTreeNode(path)).sort(fileNodesCompareFn);
 	return nodes;
 };
